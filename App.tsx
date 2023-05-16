aa90c1fd7291a969d98b206423db835e2d27c745
jawadahmed <jawadahmed@itmechanix.com>
2023-05-16 21:49:26 +0500

    Payment Method Screen

--- App.tsx
+++ App.tsx
@@ -1,10 +1,66 @@
 import React, { useState } from 'react';
 // import {Text, View} from 'react-native';
-import { View, Text, Image, Button, ScrollView, TextInput, StyleSheet, TouchableOpacity, FlatList, } from 'react-native';
+import { View, Text, Image, Button, ScrollView, TextInput, StyleSheet, TouchableOpacity, FlatList, SectionList, } from 'react-native';
+// import HeadBackground from './assets/PaymentImage/Group4107.png';
+import { RadioButton } from 'react-native-paper';
+// import RadioButton from 'react-native-radio-button';
+
+
 
 const App = () => {
   const [hungry, setHungry] = useState(true);
   const [text, setText] = useState('');
+  const [checked, setChecked] = React.useState('first');
+
+  const styles = StyleSheet.create({
+    container: {
+      height: "100%",
+      width:"100%",
+      display:'flex',
+      justifyContent:'center',
+      alignItems:'center'
+    },
+    Imagecontainer: {
+      height: "30%",
+      width:"100%",
+      display:'flex',
+      justifyContent:'center',
+      alignItems:'center'
+    },
+    paymentContainer: {
+      height: "40%",
+      width:"100%",
+      display:'flex',
+      justifyContent:'space-around',
+      alignItems:'center',
+      flexDirection:'column',
+    },
+    methodList: {
+      height: "20%",
+      width:"100%",
+      display:'flex',
+      justifyContent:'space-around',
+      alignItems:'center',
+      flexDirection:'row',
+    },
+    methodListText:{
+      fontSize:18,
+      fontWeight:'bold',
+    },
+    RadioButton:{
+    height:100,
+    width:100},
+    text:{
+      fontWeight:'bold',
+      fontSize:35,
+      color:'black'
+    },
+    item: {
+      padding: 10,
+      fontSize: 18,
+      height: 44,
+    },
+  });
   const logo = {
     uri: 'https://reactnative.dev/img/tiny_logo.png',
     width: 64,
@@ -13,20 +69,94 @@ const App = () => {
 
   return (
     <>
-      {/* FlatList */}
+      <View style={styles.container}>
+        <View style={styles.Imagecontainer}>
+      <Image
+          source={require('./assets/PaymentImage/Group4107.png')}
+          style={{width:300, height: 200 }}
+        />
+        </View>
+        <Text style={styles.text}>Choose Your Payment</Text>
+        <View style={styles.paymentContainer}>
+        <View style={styles.methodList}>
+        <Image
+          source={require('./assets/PaymentImage/money(2)1.png')}
+          style={{width:30, height: 30 }}
+        />
+        <Text style={styles.methodListText}>Cash On Arrival</Text>
+        <RadioButton
+        value="first"
+        status={ checked === 'first' ? 'checked' : 'unchecked'}
+        onPress={() => setChecked('first')}
+      />
+        </View>
+        <View style={styles.methodList}>
+        <Image
+          source={require('./assets/PaymentImage/paypal(1).png')}
+          style={{width:30, height: 30 }}
+        />
+        <Text style={styles.methodListText}>Paypal</Text>
+        <RadioButton 
+        value="first"
+        status={ checked === 'first' ? 'checked' : 'unchecked' }
+        onPress={() => setChecked('first')}
+      />
+        </View>
+        <View style={styles.methodList}>
+        <Image
+          source={require('./assets/PaymentImage/visa(1).png')}
+          style={{width:30, height: 30 }}
+        />
+        <Text style={styles.methodListText}>Visa</Text>
+        <RadioButton
+        value="first"
+        status={ checked === 'first' ? 'checked' : 'unchecked' }
+        onPress={() => setChecked('first')}
+      />
+        </View>
+        </View>
+        <View>
+   <Button onPress={()=>setHungry(true)}
+    // disabled={!hungry}
+     title={"Add Card"}   />
+    </View> 
+      </View>
+
+
+
+      {/* ************  FlatList  *********** */}
 
 
       {/* <FlatList
  data={[{key:"jawad"},{key:"jawad"},{key:"jawad"},{key:"jawad"},{key:"jawad"},{key:"jawad"},{key:"jawad"},{key:"jawad"},{key:"jawad"},{key:"jawad"},{key:"jawad"},{key:"jawad"},{key:"jawad"},{key:"jawad"},{key:"jawad"},{key:"jawad"},{key:"jawad"},{key:"jawad"},{key:"jawad"},{key:"jawad"},{key:"jawad"},{key:"jawad"},{key:"jawad"},{key:"jawad"},{key:"jawad"},{key:"jawad"},{key:"jawad"},{key:"jawad"},{key:"jawad"},{key:"jawad"},{key:"jawad"},{key:"jawad"},{key:"jawad"},{key:"jawad"},{key:"jawad"},{key:"jawad"},{key:"jawad"},{key:"jawad"},{key:"jawad"},{key:"jawad"},{key:"jawad"},{key:"jawad"},{key:"jawad"},{key:"jawad"},{key:"jawad"},{key:"jawad"},{key:"jawad"},{key:"jawad"},{key:"jawad"},{key:"jawad"},{key:"jawad"},{key:"jawad"},{key:"jawad"},{key:"jawad"},{key:"jawad"},{key:"jawad"},{key:"jawad"},{key:"jawad"},{key:"jawad"}]}
  renderItem={({item})=><Text style={{fontSize:50  ,color:'red'}}>{item.key}</Text>}
  /> */}
-      <View>
+
+
+        {/* ***********  Section List  ************** */}
+
+      {/* <View>
+      <SectionList 
+      sections={[
+        {title:"Team A",data:["jawad","Adeel","Saleem"]},{title:"Team A",data:["jawad","Adeel","Saleem"]},{title:"Team A",data:["jawad","Adeel","Saleem"]}
+      ]}
+      renderItem={({item})=><Text>{item}</Text>}
+      renderSectionHeader={({section})=><Text style={{fontSize:50}}>{section.title}</Text>}
+      keyExtractor={item=>'basicListEntry-${item}'}/>
+      </View>  */}
+
+
+
+
+
+
+      {/* <View>
    <Button onPress={()=>setHungry(false)}
     disabled={!hungry}
      title={hungry ? 'Pour me some milk, please!' : 'Thank you!'}   />
-    </View> 
+    </View>  */}
 
-      <View  style={{
+      {/* <View  style={{
         backgroundColor:"#f5f5f5",
       display:'flex',
       justifyContent:"center",
@@ -58,10 +188,10 @@ const App = () => {
         <Text style={styles.formButtonText}>Login </Text>
       </TouchableOpacity>
       <Text>Already Have an Account</Text>
-      </View>
+      </View> */}
 
 
-      {/* Text Input */}
+      {/* ***************     Text Input   ********************** */}
 
       {/* <View style={{ padding: 10 }} >
         <TextInput style={{ height: 40 }}
