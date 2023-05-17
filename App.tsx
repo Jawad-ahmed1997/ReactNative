a186076b1a6d657b7e8dbcafb101a5bb575f4e82
jawadahmed <jawadahmed@itmechanix.com>
2023-05-17 21:38:14 +0500

    Payment Method Screen complete

--- App.tsx
+++ App.tsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 // import {Text, View} from 'react-native';
-import { View, Text, Image, Button, ScrollView, TextInput, StyleSheet, TouchableOpacity, FlatList, SectionList, } from 'react-native';
+import { View, Text, Image, ScrollView, TextInput, StyleSheet, TouchableOpacity, FlatList, SectionList, } from 'react-native';
 // import HeadBackground from './assets/PaymentImage/Group4107.png';
-import { RadioButton } from 'react-native-paper';
+import { RadioButton,Button,useTheme} from 'react-native-paper';
 // import RadioButton from 'react-native-radio-button';
 
 
@@ -10,7 +10,8 @@ import { RadioButton } from 'react-native-paper';
 const App = () => {
   const [hungry, setHungry] = useState(true);
   const [text, setText] = useState('');
-  const [checked, setChecked] = React.useState('first');
+  const [checked, setChecked] = React.useState('Cash On Arrival');
+  const theme = useTheme();
 
   const styles = StyleSheet.create({
     container: {
@@ -37,11 +38,36 @@ const App = () => {
     },
     methodList: {
       height: "20%",
-      width:"100%",
+      width:"90%",
       display:'flex',
       justifyContent:'space-around',
       alignItems:'center',
       flexDirection:'row',
+      backgroundColor:"#ffffff",borderRadius:10
+    },
+    methodListImage:{
+      width:"20%",
+      height:"100%",
+      display:'flex',
+      justifyContent:'center',
+      alignItems:"flex-start",
+      paddingLeft:10
+    },
+    methodListTextView:{
+      width:"60%",
+      height:"100%",
+      display:'flex',
+      justifyContent:'center',
+      alignItems:"flex-start",
+      paddingLeft:10
+    },
+    methodListRadio:{
+      width:"20%",
+      height:"100%",
+      display:'flex',
+      justifyContent:'center',
+      alignItems:"flex-end",
+      paddingRight:10
     },
     methodListText:{
       fontSize:18,
@@ -52,8 +78,34 @@ const App = () => {
     width:100},
     text:{
       fontWeight:'bold',
-      fontSize:35,
+      fontSize:31,
       color:'black'
+    },
+    AddCardButton: {
+      borderRadius:10,
+      borderColor:'#308bfd',
+   
+    },
+    cotinueButton: {
+      borderRadius:10,
+      borderColor:'#308bfd',
+      paddingLeft:70,
+      paddingRight:70,
+      color:'#FFFFFF',
+      backgroundColor:"#308bfd"
+    },
+    ButtonView:{
+      display:'flex',
+      justifyContent:'center',
+      alignItems:"flex-end",
+      marginRight:"10%",width:"100%"
+    },
+    ContinueButtonView:{
+      height:'10%',
+      display:'flex',
+      justifyContent:'center',
+      alignItems:"center",
+      width:"100%"
     },
     item: {
       padding: 10,
@@ -79,47 +131,84 @@ const App = () => {
         <Text style={styles.text}>Choose Your Payment</Text>
         <View style={styles.paymentContainer}>
         <View style={styles.methodList}>
-        <Image
+          <View style={styles.methodListImage}>
+          <Image 
           source={require('./assets/PaymentImage/money(2)1.png')}
           style={{width:30, height: 30 }}
         />
-        <Text style={styles.methodListText}>Cash On Arrival</Text>
-        <RadioButton
-        value="first"
-        status={ checked === 'first' ? 'checked' : 'unchecked'}
-        onPress={() => setChecked('first')}
+          </View>
+          <View style={styles.methodListTextView}>
+          <Text style={styles.methodListText}>Cash On Arrival</Text>
+          </View>
+          <View style={styles.methodListRadio}>
+          <RadioButton
+        value="Cash On Arrival"
+        status={ checked === 'Cash On Arrival' ? 'checked' : 'unchecked'}
+        onPress={() => setChecked('Cash On Arrival')}
       />
         </View>
+      
+        </View>
         <View style={styles.methodList}>
-        <Image
+          <View style={styles.methodListImage}>
+          <Image 
           source={require('./assets/PaymentImage/paypal(1).png')}
           style={{width:30, height: 30 }}
         />
-        <Text style={styles.methodListText}>Paypal</Text>
-        <RadioButton 
-        value="first"
-        status={ checked === 'first' ? 'checked' : 'unchecked' }
-        onPress={() => setChecked('first')}
+          </View>
+          <View style={styles.methodListTextView}>
+          <Text style={styles.methodListText}>Paypal</Text>
+          </View>
+          <View style={styles.methodListRadio}>
+          <RadioButton.Android 
+  style={styles.RadioButton}
+  value="Paypal"
+  status={ checked === 'Paypal' ? 'checked' : 'unchecked'}
+  onPress={() => setChecked('Paypal')}
       />
         </View>
+      
+        </View>
         <View style={styles.methodList}>
-        <Image
+          <View style={styles.methodListImage}>
+          <Image 
           source={require('./assets/PaymentImage/visa(1).png')}
           style={{width:30, height: 30 }}
         />
-        <Text style={styles.methodListText}>Visa</Text>
-        <RadioButton
-        value="first"
-        status={ checked === 'first' ? 'checked' : 'unchecked' }
-        onPress={() => setChecked('first')}
+          </View>
+          <View style={styles.methodListTextView}>
+          <Text style={styles.methodListText}>Visa</Text>
+          </View>
+          <View style={styles.methodListRadio}>
+          <RadioButton
+        value="Visa"
+        status={ checked === 'Visa' ? 'checked' : 'unchecked'}
+        onPress={() => setChecked('Visa')}
       />
         </View>
+      
         </View>
-        <View>
-   <Button onPress={()=>setHungry(true)}
+        </View>
+       <View style={styles.ButtonView}>
+       <Button 
+   style={styles.AddCardButton}
+      labelStyle={{
+        fontWeight: 'bold',
+        fontSize: 14,
+      }}  onPress={()=>setHungry(true)}
     // disabled={!hungry}
-     title={"Add Card"}   />
-    </View> 
+    mode="outlined" textColor="#308bfd" >Add Card</Button>
+       </View>
+          <View style={styles.ContinueButtonView}>
+          <Button 
+   style={styles.cotinueButton}
+      labelStyle={{
+        fontWeight: 'bold',
+        fontSize: 18,
+      }}  onPress={()=>setHungry(true)}
+    disabled={!hungry}
+    mode="elevated" textColor="#FFFFFF" >Continue</Button>
+          </View>
       </View>
 
 
